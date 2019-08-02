1d418d7b086f513a38ba2b72e8d66b93ad33ada8
parzivalEduardo <dev_eduardo@hotmail.com>
2019-08-02 00:00:40 -0300

    removendo método update.

--- client/js/app/views/NegociacoesView.js
+++ client/js/app/views/NegociacoesView.js
@@ -1,10 +1,10 @@
-class NegociacoesView {
+class NegociacoesView extends View {
 
-    constructor(elemento) {
-        this._elemento = elemento;
+    constructor(elemento){
+        super(elemento);
     }
 
-    _template(model) {
+    template(model) {
 
         return `
         
@@ -37,9 +37,4 @@ class NegociacoesView {
         
         `
     }
-
-    update(model) {
-        this._elemento.innerHTML = this._template(model);
-    }
-
 }
