5a7ce377cfde907a4097b0a54db824e244a259e6
parzivalEduardo <dev_eduardo@hotmail.com>
2019-08-03 12:23:56 -0300

    add método armadilha de update de negociacoesView.

--- client/js/app/models/ListaNegociacoes.js
+++ client/js/app/models/ListaNegociacoes.js
@@ -1,14 +1,21 @@
 class ListaNegociacoes {
 
-    constructor () {
+    constructor (armadilha) {
         this._negociacoes = [];
-    }
-
-    adiciona (negociacao) {
-        this._negociacoes.push(negociacao);
+        this._armadilha = armadilha;
     }
 
     get negociacoes () {
         return [].concat(this._negociacoes);
+    }
+
+    adiciona (negociacao) {
+        this._negociacoes.push(negociacao);
+        this._armadilha(this);
+    }
+
+    esvazia () {
+        this._negociacoes = [];
+        this._armadilha(this);
     }
 }
