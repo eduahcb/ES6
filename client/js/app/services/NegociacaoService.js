d0470c9a4195d882a645ec6f53772428bf664ff7
Eduardo <dev_eduardo@hotmail.com>
2019-09-07 09:48:38 -0300

    criando um método de cadastrar a negociação no banco.

--- client/js/app/services/NegociacaoService.js
+++ client/js/app/services/NegociacaoService.js
@@ -58,4 +58,18 @@ class NegociacaoService {
         });
 
     }
+
+    cadastra(negociacao){
+
+        return ConnectionFactory
+            .getConnection()
+            .then(connection => new NegociacaoDao(connection))
+            .then( dao => dao.adiciona(negociacao))
+            .then( () => 'Negociacao adicionada com sucesso')
+            .catch( (erro) => {
+                console.log(erro);
+                throw new Error('Não foi possível adicionar a negociação') 
+            });
+
+    }
 }
