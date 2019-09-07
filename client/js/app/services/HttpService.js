4bc5c610b04a42a378fdf1ef614f4f306764d0a3
Eduardo <dev_eduardo@hotmail.com>
2019-09-07 11:57:53 -0300

    utilizando o fetch API.

--- client/js/app/services/HttpService.js
+++ client/js/app/services/HttpService.js
@@ -3,56 +3,26 @@ class HttpService {
 
     get(url) {
 
-        return new Promise( (resolve, reject) =>{
+        return fetch(url)
+            .then(res => res.json())
+            .then(data => data)
+            .catch(erro => erro);
 
-            let xhr = new XMLHttpRequest;
-
-            xhr.open('GET', url);
-
-            xhr.onreadystatechange = () => {
-
-                if (xhr.readyState === 4) {
-
-                    if (xhr.status === 200)
-
-                        resolve(JSON.parse(xhr.responseText));
-
-                    else
-                        reject(JSON.parse(xhr.responseText));
-                }
-            }
-
-            xhr.send();
-
-        });
     }
 
     post(url, dado) {
 
-        return Promise( (resolve, reject) => {
+        return fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(dado)
+            })
+            .then(res => res.json())
+            .then(dado => dado)
+            .catch(erro => erro);
 
-            let xhr = new XMLHttpRequest;
-
-            xhr.open('POST', url);
-
-            xhr.setRequestHeader('Content-type', 'application/json');
-
-            xhr.onreadystatechange = () => {
-
-                if (xhr.readyState === 4) {
-
-                    if (xhr.status === 200)
-
-                        resolve(JSON.parse(xhr.responseText));
-
-                    else
-                        reject(JSON.parse(xhr.responseText));
-                }
-            }
-
-            xhr.send(JSON.stringify(dado));
-
-        })
     }
 
 }
